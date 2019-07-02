45c9758415923267c56872fd24cb01a06e28cd34
dishant <dishantgandhi99@yahoo.com>
2019-07-02 15:22:52 +0530

    changed js/script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-function revealMessage(num) {
-  if(num == 1){
-  //var x = document.getElementsByClassName("box");
-  var x = document.getElementById('skill');
-  x.style.display ='block';
-  document.getElementById('activity').style.display = "none";
-  document.getElementById('project').style.display ="none";
-
-
-}
-else if (num == 2) {
-  //var x = document.getElementsByClassName("box2");
-  //document.getElementById('skill').style.display="none";
-  document.getElementById('project').style.display = "block";
-  document.getElementById('skill').style.display="none";
-  document.getElementById("activity").style.display = "none";
-
-}
-else{
-  document.getElementById("activity").style.display = "block";
-  document.getElementById('skill').style.display="none";
- document.getElementById('project').style.display ="none";
-}
-}
+document.addEventListener('DOMContentLoaded',function (){
+  document.querySelector('#b1').onclick = function (){
+    document.getElementById('skill').style.display = "block";
+    document.getElementById('activity').style.display = "none";
+    document.getElementById('project').style.display ="none";
+  }
+  document.querySelector('#b2').onclick = function(){
+    document.getElementById('project').style.display = "block";
+    document.getElementById('skill').style.display="none";
+    document.getElementById("activity").style.display = "none";
+  }
+  document.querySelector('#b3').onclick = function(){
+    document.getElementById("activity").style.display = "block";
+    document.getElementById('skill').style.display="none";
+    document.getElementById('project').style.display ="none";
+  }
+})
