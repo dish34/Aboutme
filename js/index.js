d65a04e4c93e147bc84797fbb897b4691a8947f5
dishant <dishantgandhi99@yahoo.com>
2019-09-08 21:56:37 +0530

    seo improvments

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var typed3 = new Typed('#text', {
-    strings: [' ', ' <strong>Competitive Programmer</strong>', '<strong>Django Lover</strong>' , '<strong>Quora Writer</strong>'],
+    strings: [' ', ' <strong>Competitive Programmer</strong>', '<strong>Django Lover</strong>' , '<strong>Quora Writer</strong>' ,'<strong>A Friend</strong>'],
     typeSpeed: 100,
     backSpeed: 150,
     smartBackspace: true, // this is a default
